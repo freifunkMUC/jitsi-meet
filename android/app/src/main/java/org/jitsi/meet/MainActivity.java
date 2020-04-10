8408076ad19bfbef92c26e6f70bbf37959427fa5
T0biii <tobias-m99@gmx.de>
2020-04-10 03:30:34 +0200

    Android: Change to FFMUC Branding

--- android/app/src/main/java/org/jitsi/meet/MainActivity.java
+++ android/app/src/main/java/org/jitsi/meet/MainActivity.java
@@ -89,7 +89,7 @@ public class MainActivity extends JitsiMeetActivity {
         JitsiMeetConferenceOptions defaultOptions
             = new JitsiMeetConferenceOptions.Builder()
                 .setWelcomePageEnabled(true)
-                .setServerURL(buildURL("https://meet.jit.si"))
+                .setServerURL(buildURL("https://meet.ffmuc.net"))
                 .setFeatureFlag("call-integration.enabled", false)
                 .build();
         JitsiMeet.setDefaultConferenceOptions(defaultOptions);
