d10c63195a20541a628a76a81901b18ddfdf3073
Tobias <T0biii@users.noreply.github.com>
2020-04-11 22:28:43 +0200

    Update build-apk.sh

--- android/scripts/build-apk.sh
+++ android/scripts/build-apk.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-cd ../../
+
+THIS_DIR=$(cd -P "$(dirname "$(readlink "${BASH_SOURCE[0]}" || echo "${BASH_SOURCE[0]}")")" && pwd)
+cd ${THIS_DIR}../../
 react-native bundle --platform android --dev false --entry-file index.android.js --bundle-output android/app/src/main/assets/index.android.bundle --assets-dest android/app/src/main/res
 cd android
 ./gradlew assembleDebug
