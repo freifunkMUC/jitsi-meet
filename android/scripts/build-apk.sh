ace9bd9bd75ecf124972424433e7a2d8de5a46e4
Tobias <tobias-m99@gmx.de>
2020-04-11 22:34:47 +0200

    Revert "Update build-apk.sh"
    This reverts commit d10c63195a20541a628a76a81901b18ddfdf3073.

--- android/scripts/build-apk.sh
+++ android/scripts/build-apk.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-
-THIS_DIR=$(cd -P "$(dirname "$(readlink "${BASH_SOURCE[0]}" || echo "${BASH_SOURCE[0]}")")" && pwd)
-cd ${THIS_DIR}../../
+cd ../../
 react-native bundle --platform android --dev false --entry-file index.android.js --bundle-output android/app/src/main/assets/index.android.bundle --assets-dest android/app/src/main/res
 cd android
 ./gradlew assembleDebug
